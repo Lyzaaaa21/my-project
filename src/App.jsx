14dc0ef1de0426889a4522fbe7d655cb8be75281
Lyzaaaa21 <148184575+Lyzaaaa21@users.noreply.github.com>
2024-09-16 19:06:49 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// function App() {
+
+const App = () => {
+  // const [count, setCount] = useState(0)
+
+  return <section className="sm:max-xl:bg-blue-300 md:grid-cols-2 md:items-center md:text-left 
+  sm:max-xl:p-9 grid gap-8 ">   
+      <div>
+          <img src="https://th.bing.com/th/id/OIP.QsYBu6AtkeHOTImicl3MUgHaEK?rs=1&pid=ImgDetMain"
+          className='w-full rounded-lg'
+          />
+
+      </div>
+      <div>
+
+       <h1 className="mb-2 text-4xl font-medium">My Beloved Stranger</h1>
+
+        <p>Nezuko is a young girl with fair skin, visibly large, prominent fangs,
+         a slender build, and sharp, stiletto nails with a base color of light pink
+          and ombre fading to a red-pinkish color at the end where they point. 
+          Her hair is long, black, and wavy. It reaches just below her waist, turning
+         a flame-orange color as it passes her elbows, and appears to be crimped into 
+          large, straight ridges, worn side-swept drastically to her left. She has soft-looking,
+          pale pink eyes that appear a lighter color around the rims of their irises and on the
+          center a persian pink below the pupils; they slanted downwards toward the sides of her face 
+         and framed by notably long eyelashes. Her pupils appear as black slits as she transforms. 
+         In the manga, Nezuko can sometimes be shown with more than one bow. According to Tanjiro, 
+          Nezuko was known as a great beauty in their home town.She wears a light pink kimono with an
+          asanoha hemp leaf pattern, the lining a paler pink layered with the white yuban underneath as a short trailing skirt kimono
+         to freely move as a demon, secured by a red and white-checkered hanhaba obi with an orange-threaded obijime and a green obiage.
+         Over this, she sports a long, dark-brown haori that reaches her calves, as well as a pair of zōri with pink straps and white tabi 
+         socks, wrapped with thick pieces of black material resembling kyahan. Nezuko also wears a small pink ribbon on the left side of
+         her head to keep her hair out of her face, along with the bamboo muzzle Giyu Tomioka gave her, which is secured around her
+         jaw with a strip of red cloth.</p>
+
+         
+      </div>
+
+
+
+  </section>
+ 
+}
+
+export default App
